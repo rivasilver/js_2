1fd51f32d6e490e5aa8ab02399b271ed5f1b65fa
Vágó Rebeka <rebeka.vago19@gmail.com>
2021-12-08 23:20:46 +0100

    4. feladat félig kész

---
+++ szamok.js
@@ -0,0 +1,11 @@
+function szamKiir(){
+    const szamok = []
+    for (var i = 1; i < 21; i++)
+    szamok.push(i);
+    // for (var i = 1; i < 20; i+2){
+    //     if (szamok[i] % 2 != 0){
+    //         szamok[i] = id="paratlan";
+    //     }
+    // }
+    return szamok
+}
